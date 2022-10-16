a2b1b80aab2355615dd5f81b80b9fbdd2949961c
Apsapeh <timofeykirichenko@gmail.com>
2022-10-16 17:33:24 +0300

    CUMmit

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -2,5 +2,24 @@
 
 Parser::Parser(std::string lexedString) {
 
-    // Тут были написаны глупости
+    // Тут были написаны глупостиr fdpoi poidpso gipofdi poipf  aofjoifofjs doifsej lksdjfoiesjafljsdfl;i jsal;kfjs
+    f\
+    sadf lksjdf l;kno;sdj f
+    ]sd 'jfs;kdaj tgp[o'aesk;l
+    f djd
+    'pfj[ejfoli;dasjtg asd] 'jasd'j'opweajdsf
+    csda gjo;sfpocjkaosdtgu pjl;k]
+    d
+    skdfl;kjd;ljsio;fgi[0sraayth
+    ]afjdsp
+    'fp[sjf i;osaeuptrjsa;lksdtg'poesjrl;fkhsd
+    ]sdpjgsdpa]irsdjfvlkjfa
+    ew
+    sjtp'osdkgflkhjsdpof ksl'adh
+    'ps;alhfds';oghsa'djfkjsahfp;sdajtio;gesa';ds
+    [sdaiohjtew
+    'ifkl;sdjhiljsakjhtwd;dasf as'ehtds]fkjhdsajkgoseau
+    sd'fhjsda'piglsdhoitlahbfsdoijsda
+    asjt[sdiflkdsh9prfrlsaehfp
+    'sdnat'asifklk;nvoisdaitnj]
 }
